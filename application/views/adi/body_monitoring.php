87b9630d202112aa711e71eeabe1b327db393b00
unknown <adipradipta93@gmail.com>
2017-09-12 12:25:10 +0700

    on dev

--- application/views/adi/body_monitoring.php
+++ application/views/adi/body_monitoring.php
@@ -11,7 +11,7 @@
                                 </li>
                             </ul>
                         </div>
-                        <h1 class="page-title"> Monitoring Ketinggian Darrma
+                        <h1 class="page-title"> Monitoring Ketinggian Darrma hehehehehe
                             <small>Sensor Ultrasonic</small>
                         </h1>
                         <div class="container-fluid">
